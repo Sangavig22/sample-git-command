4df6431b89a30889b87c9be43ca399d4f9449da1
Sangavig22 <sangavi200209@gmail.com>
2025-10-27 10:36:11 +0530

    message

---
+++ bank_app/src/Apps.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+
+const App=()=>{
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline text-blue-600">
+       Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
